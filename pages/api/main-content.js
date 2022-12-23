3b90190e24566699870fd58ddf62f372b8f0d8be
craiglarrabee <craiglarrabee@churchofjesuschrist.org>
2022-12-23 12:43:17 -0700

    test SignatureV4

--- pages/api/main-content.js
+++ pages/api/main-content.js
@@ -1,8 +1,22 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
+const { SignatureV4 } = require("@aws-sdk/signature-v4");
+const { fromNodeProviderChain } = require("@aws-sdk/credential-providers");
+const { Sha256 } = require("@aws-crypto/sha256-js");
+const {
+    AWS_REGION
+} = process.env;
+const creds = fromNodeProviderChain();
+const signer = new SignatureV4({
+    service: "execute-api",
+    region: AWS_REGION,
+    credentials: creds,
+    sha256: Sha256,
+});
+
 export default async function handler(req, res) {
     console.log(req);
-    console.log(process.env);
+    console.log(signer.toString());
     if (req.method === "POST") {
         // await DataStore.save(
         //     new HomePageContent({
