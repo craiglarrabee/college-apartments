07a5b9f0a1dc4fe15f6e10c8af6a593a7a66e5a1
Craig <craiglarrabee@churchofjesuschrist.org>
2024-01-15 15:01:39 -0700

    make payments link dependent on having a processed application in the system

--- pages/api/login.js
+++ pages/api/login.js
@@ -1,12 +1,14 @@
 import {withIronSessionApiRoute} from "iron-session/next";
 import {ironOptions} from "../../lib/session/options";
 import {GetUserAdminSites, GetUserAndVerifyPassword} from "../../lib/db/users/user";
+import {GetTenantProcessedApplicationSites} from "../../lib/db/users/application";
 
 const login = withIronSessionApiRoute(async (req, res) => {
     // get user from database
     try {
         const userData = await GetUserAndVerifyPassword(req.body.username, req.body.password);
         const data = await GetUserAdminSites(userData.id);
+        const processedForSites = await GetTenantProcessedApplicationSites(userData.id, req.query.site);
         const adminSites = data.filter(site => site.site_privs === 1).map(site => site.site);
         const manageSites = data.filter(site => site.apartment_privs === 1).map(site => site.site);
         const user = {
@@ -17,7 +19,8 @@ const login = withIronSessionApiRoute(async (req, res) => {
             admin: adminSites,
             manage: manageSites,
             editSite: false,
-            email: userData.email
+            email: userData.email,
+            processedSites: processedForSites
         };
         req.session.user = user;
         await req.session.save();
