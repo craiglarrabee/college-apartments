7c70a7619297600baf28cf48b4f47ad555964471
Craig <craiglarrabee@churchofjesuschrist.org>
2026-07-17 15:16:38 -0600

    fix bug adding userLease

--- components/newApplicationForm.jsx
+++ components/newApplicationForm.jsx
@@ -26,6 +26,7 @@ const NewApplicationForm = ({
                                 tenant,
                                 isReturningStudent,
                                 isDepositPaid,
+                                isOptional,
                                 depositAmount,
                                 privacyContent,
                                 refundContent,
@@ -265,6 +266,13 @@ const NewApplicationForm = ({
         setApplicationError(null);
 
         let shouldPay = depositRequired;
+        if (isOptional && depositRequired) {
+            if (useSquare) {
+                shouldPay = isSquareValid;
+            } else {
+                shouldPay = !!(data.cc_number || data.cc_expire || data.cc_code);
+            }
+        }
 
         if (useSquare && shouldPay) {
             const squareOk = await checkSquareValues();
@@ -491,11 +499,11 @@ const NewApplicationForm = ({
                                 </Form.Group>
                             </Row>
                         )}
-                        <p>{`A deposit of ${currency.format(dynamicDepositAmount)} is required.`}{site === "snow" && ` A 2.75% processing fee (${currency.format(dynamicDepositAmount * 0.0275)}) will be added for card payments, for a total of ${currency.format(dynamicDepositAmount * 1.0275)}.`}</p>
+                        <p>{isOptional ? `A deposit of ${currency.format(dynamicDepositAmount)} is optional.` : `A deposit of ${currency.format(dynamicDepositAmount)} is required.`}{site === "snow" && ` A 2.75% processing fee (${currency.format(dynamicDepositAmount * 0.0275)}) will be added for card payments, for a total of ${currency.format(dynamicDepositAmount * 1.0275)}.`}</p>
                         
                         {useSquare ? (
                             <>
-                                <Form.Label className="required">Card Information</Form.Label>
+                                <Form.Label className={!isOptional && "required"}>Card Information</Form.Label>
                                 <Row>
                                     <Col xs={12} className="mb-3">
                                         <div id="sq-card-container" style={{border: '1px solid #ced4da', borderRadius: 4, padding: 12}} />
@@ -505,13 +513,13 @@ const NewApplicationForm = ({
                             </>
                         ) : (
                             <>
-                                <Form.Label className="required">Credit Card Information</Form.Label>
+                                <Form.Label className={!isOptional && "required"}>Credit Card Information</Form.Label>
                                 <Row>
                                     <Form.Group as={Col} xs={12} md={6} className="mb-3" controlId="cc_number">
-                                        <Form.Label className="required">Card Number</Form.Label>
+                                        <Form.Label className={!isOptional && "required"}>Card Number</Form.Label>
                                         <Form.Control maxLength={19} autoComplete="cc-number"
                                                       className={errors && errors.cc_number && classNames("border-danger")} {...register("cc_number", {
-                                                required: depositRequired ? "Card Number is required" : false,
+                                                required: (depositRequired && !isOptional) ? "Card Number is required" : false,
                                                 pattern: {
                                                     value: /\d{4} \d{4} \d{4} \d{3,4}/,
                                                     message: "Valid Card Number is required"
@@ -521,10 +529,10 @@ const NewApplicationForm = ({
                                         {errors && errors.cc_number && <Form.Text className="text-danger">{errors.cc_number.message}</Form.Text>}
                                     </Form.Group>
                                     <Form.Group as={Col} xs={6} md={3} className="mb-3" controlId="cc_expire">
-                                        <Form.Label className="required">Expires</Form.Label>
+                                        <Form.Label className={!isOptional && "required"}>Expires</Form.Label>
                                         <Form.Control maxLength={7} autoComplete="cc-exp"
                                                       className={errors && errors.cc_expire && classNames("border-danger")} {...register("cc_expire", {
-                                            required: depositRequired ? "MM/YYYY is required" : false,
+                                            required: (depositRequired && !isOptional) ? "MM/YYYY is required" : false,
                                             pattern: {
                                                 value: /\d{2}\/\d{4}/,
                                                 message: "MM/YYYY is required"
@@ -534,10 +542,10 @@ const NewApplicationForm = ({
                                         {errors && errors.cc_expire && <Form.Text className="text-danger">{errors.cc_expire.message}</Form.Text>}
                                     </Form.Group>
                                     <Form.Group as={Col} xs={6} md={3} className="mb-3" controlId="cc_code">
-                                        <Form.Label className="required">CCV</Form.Label>
+                                        <Form.Label className={!isOptional && "required"}>CCV</Form.Label>
                                         <Form.Control maxLength={4}
                                                       className={errors && errors.cc_code && classNames("border-danger")} {...register("cc_code", {
-                                            required: depositRequired ? "CVV is required" : false,
+                                            required: (depositRequired && !isOptional) ? "CVV is required" : false,
                                             pattern: {
                                                 value: /\d{3,4}/,
                                                 message: "Valid CVV is required"
@@ -567,7 +575,7 @@ const NewApplicationForm = ({
                                                     </span>
                                                 }
                                                 {...register("agreeToPolicies", {
-                                                    required: depositRequired ? "You must agree to the privacy and refund policies to continue." : false
+                                                    required: (depositRequired && !isOptional) ? "You must agree to the privacy and refund policies to continue." : false
                                                 })}
                                                 isInvalid={!!errors.agreeToPolicies}
                                             />
@@ -597,7 +605,7 @@ const NewApplicationForm = ({
                 <div style={{width: "100%"}}
                      className={classNames("mb-3", "justify-content-center", "d-inline-flex", "mt-4")}>
                     <Button variant="primary" type="submit" disabled={canEdit || isProcessing || !isValid}>
-                        {isProcessing ? "Processing..." : (depositRequired ? "Pay Deposit and Submit Application" : "Submit")}
+                        {isProcessing ? "Processing..." : ((depositRequired && !isOptional) ? "Pay Deposit and Submit Application" : "Submit")}
                     </Button>
                 </div>
             </Form>
--- pages/tenants/[userId].js
+++ pages/tenants/[userId].js
@@ -525,6 +525,7 @@ const Tenant = ({
                                                                 previous_rental={applicationContent.previous_rental}
                                                                 isReturningStudent={restOfProps.isReturningStudent}
                                                                 isDepositPaid={restOfProps.isDepositPaid}
+                                                                isOptional={!isTenant}
                                                                 depositAmount={restOfProps.depositAmount}
                                                                 privacyContent={restOfProps.privacyContent}
                                                                 refundContent={restOfProps.refundContent}
